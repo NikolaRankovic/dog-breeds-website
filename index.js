456ef709747b5e938855b30947bffdc4ba2a5f09
Nikola Rankovic <nkl.rnk97@gmail.com>
2021-10-26 10:26:27 +0200

    Add initial project files

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("#dugme").addEventListener("click", function () {
+    alert("Cao");
+});
+
+document.querySelector(".vojin").addEventListener("mouseenter", function () {
+    alert("Nikola Rankovic");
+});
